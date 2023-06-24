42e847a3003a2818d2ff391d6fb787e50f63edf6
Frank Wu <fwu2mobile@gmail.com>
2023-06-24 11:30:29 -0400

    20230624

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts"
+import MenuBar from "./MenuBar";
+
+function CreateCard(contact) {
+
+  return <Card 
+    key = {contact.id}
+    name = {contact.name}
+    img = {contact.imgURL }
+    tel = {contact.phone}
+    email = {contact.email}
+  />
+}
 
 function App() {
 
   return (
     <div>
-      <h1 className="heading">TukTuk</h1>
-      <Card 
-        name = {contacts[0].name}
-        img= {contacts[0].imgURL}
-        tel = {contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card 
-        name = {contacts[1].name}
-        img= {contacts[1].imgURL}
-        tel = {contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card 
-      name = {contacts[2].name}
-      img= {contacts[2].imgURL}
-      tel = {contacts[2].phone}
-      email={contacts[2].email}
-    />
-    
+      <h1 className="heading">Resturant Name</h1>
+
+      {contacts.map(CreateCard)}
+      
+      <MenuBar />
+
     </div>
   );
 }
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+function MenuBar(){
+
+    return 
+}
+
+export default MenuBar;
